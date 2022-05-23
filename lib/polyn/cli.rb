a1a176edd87a5a4790fed8ec480f8cdd7ee94329
Jarod <therealfugu@gmail.com>
2022-05-23 17:56:27 -0600

    init

---
+++ lib/polyn/cli.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "cli/version"
+
+module Polyn
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/polyn/cli/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Polyn
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
