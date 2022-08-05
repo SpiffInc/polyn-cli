23e6c4a289ce1c6279ed2bfeb54678602a50fc35
brandynbennett <brandynlbennett@gmail.com>
2022-08-05 15:39:20 -0600

    first schema_loader test

--- lib/polyn/cli/schema_loader.rb
+++ lib/polyn/cli/schema_loader.rb
@@ -16,20 +16,19 @@ module Polyn
         new(cli).load_events
       end
 
-      def initialize(thor)
+      def initialize(thor, **opts)
         @thor               = thor
         @client             = NATS.connect(Polyn::Cli.configuration.nats_servers).jetstream
-        @bucket             = client.key_value(STORE_NAME)
+        @bucket             = client.key_value(opts.fetch(:store_name, STORE_NAME))
         @cloud_event_schema = Polyn::Cli::CloudEvent.to_h.freeze
-
-        @events = {}
+        @events_dir         = opts.fetch(:events_dir, File.join(Dir.pwd, "events"))
+        @events             = {}
       end
 
       def load_events
         thor.say "Loading events into the Polyn event registry from '#{events_dir}'"
         read_events
 
-        puts events
         events.each do |name, event|
           bucket.put(name, JSON.generate(event))
         end
@@ -39,22 +38,18 @@ module Polyn
 
       private
 
-      attr_reader :thor, :events, :client, :bucket, :cloud_event_schema
+      attr_reader :thor, :events, :client, :bucket, :cloud_event_schema, :events_dir
 
       def read_events
         Dir.glob(File.join(events_dir, "*.json")).each do |event_file|
           thor.say "Loading 'event #{event_file}'"
-          event = wrap_cloud_event(JSON.parse(File.read(event_file)))
+          event = compose_cloud_event(JSON.parse(File.read(event_file)))
 
           events[File.basename(event_file, ".json")] = event
         end
       end
 
-      def events_dir
-        File.join(Dir.pwd, "events")
-      end
-
-      def wrap_cloud_event(event_schema)
+      def compose_cloud_event(event_schema)
         cloud_event_schema.merge({
           "definitions" => cloud_event_schema["definitions"].merge({
             "datadef" => event_schema,
--- spec/polyn/cli/schema_loader_spec.rb
+++ spec/polyn/cli/schema_loader_spec.rb
@@ -2,10 +2,62 @@
 
 require "spec_helper"
 
-RSpec.describe Polyn::Cli::CloudEvent do
-  describe "#to_h" do
-    it "returns the cloud event schema as a hash" do
-      expect(described_class.to_h).to be_a(Hash)
+class FakeThor < Thor
+end
+
+RSpec.describe Polyn::Cli::SchemaLoader do
+  describe "#load_events" do
+    include_context :tmp_dir
+
+    let(:store_name) { "SCHEMA_LOADER_TEST_STORE" }
+    let(:thor) { double("thor") }
+    let(:nats) { NATS.connect }
+    let(:js) { nats.jetstream }
+
+    before(:each) do
+      allow(thor).to receive(:say)
+      js.create_key_value(bucket: store_name)
+    end
+
+    after(:each) do
+      js.delete_key_value(store_name)
+    end
+
+    subject do
+      described_class.new(thor,
+        store_name: store_name,
+        events_dir: tmp_dir)
+    end
+
+    it "it loads events to the store" do
+      add_schema_file("app.widgets.v1", {
+        "type"       => "object",
+        "properties" => {
+          "name" => { "type" => "string" },
+        },
+      })
+
+      subject.load_events
+
+      schema  = get_schema("app.widgets.v1")
+      datadef = schema["definitions"]["datadef"]
+      expect(datadef).to eq({
+        "type"       => "object",
+        "properties" => {
+          "name" => { "type" => "string" },
+        },
+      })
+    end
+
+    def add_schema_file(name, content)
+      path = File.join(tmp_dir, "#{name}.json")
+      File.write(path, JSON.generate(content))
+    end
+
+    def get_schema(name)
+      kv    = js.key_value(store_name)
+      entry = kv.get(name)
+      JSON.parse(entry.value)
     end
   end
 end
