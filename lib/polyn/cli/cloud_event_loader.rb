5572cf0a04d9b9332cd2305b6b340c130c15721a
brandynbennett <brandynlbennett@gmail.com>
2022-08-05 14:50:30 -0600

    schemas get added

--- lib/polyn/cli/cloud_event_loader.rb
+++ lib/polyn/cli/cloud_event_loader.rb
@@ -5,48 +5,9 @@ module Polyn
     ##
     # Loads the JSON schmea into the event registry.
     class CloudEventLoader
-      STORE_NAME = "POLYN_SCHEMAS"
+      include Thor::Actions
 
-      CLOUD_EVENT_SCHEMA = {
-        "$schema"    => "http://json-schema.org/draft-04/schema#",
-        "$id"        => "https://raw.githubusercontent.com/cloudevents/spec/v1.0.1/spec.json",
-        "properties" => {
-          "polyndata"  => {
-            "type"       => "object",
-            "properties" => {
-              "lang"        => {
-                "type" => "string",
-              },
-              "langversion" => {
-                "type" => "string",
-              },
-              "version"     => {
-                "type" => "string",
-              },
-            },
-            "required"   => %w[lang langversion version],
-          },
-          "polyntrace" => {
-            "type"  => "array",
-            "items" => {
-              "type"       => "object",
-              "properties" => {
-                "type" => {
-                  "type" => "string",
-                },
-                "time" => {
-                  "type" => "date-time",
-                },
-                "id"   => {
-                  "type" => "uuid",
-                },
-              },
-              "required"   => %w[type time id],
-            },
-          },
-        },
-        "required"   => %w[id type source specversion datacontenttype type data],
-      }.freeze
+      STORE_NAME = "POLYN_SCHEMAS"
 
       ##
       # Loads the events from the event repository into the Polyn event registry.
@@ -56,20 +17,21 @@ module Polyn
       end
 
       def initialize(thor)
-        @thor   = thor
-        @client = NATS.connect(Polyn::Cli.configuration.nats_servers).jetstream
-        @bucket = client.key_value(STORE_NAME)
+        @thor               = thor
+        @client             = NATS.connect(Polyn::Cli.configuration.nats_servers).jetstream
+        @bucket             = client.key_value(STORE_NAME)
+        @cloud_event_schema = Polyn::Cli::CloudEvent.to_h.freeze
 
         @events = {}
       end
 
-      # @private
       def load_events
         thor.say "Loading events into the Polyn event registry from '#{events_dir}'"
         read_events
 
+        puts events
         events.each do |name, event|
-          bucket.put(name, event)
+          bucket.put(name, JSON.generate(event))
         end
 
         true
@@ -77,27 +39,27 @@ module Polyn
 
       private
 
+      attr_reader :thor, :events, :client, :bucket, :cloud_event_schema
+
       def read_events
         Dir.glob(File.join(events_dir, "*.json")).each do |event_file|
           thor.say "Loading 'event #{event_file}'"
           event = wrap_cloud_event(JSON.parse(File.read(event_file)))
 
-          events[event[File.basename(event_file, ".json")]] = event
+          events[File.basename(event_file, ".json")] = event
         end
       end
-
-      attr_reader :thor, :events, :client, :bucket
 
       def events_dir
         File.join(Dir.pwd, "events")
       end
 
-      def wrap_cloud_event(event)
-        CLOUD_EVENT_SCHEMA.merge(
-          "properties" => CLOUD_EVENT_SCHEMA["properties"].merge(
-            "data" => event,
-          ),
-        )
+      def wrap_cloud_event(event_schema)
+        cloud_event_schema.merge({
+          "definitions" => cloud_event_schema["definitions"].merge({
+            "datadef" => event_schema,
+          }),
+        })
       end
     end
   end
